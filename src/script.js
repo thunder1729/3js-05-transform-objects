dea35b9d4ec9df524e8ee48a8046026cc3f18dcc
BrickBoy <112133175+BrickBoy1432@users.noreply.github.com>
2022-12-26 19:02:12 -0800

    My first commit
    nothing really to say here :)

---
+++ src/script.js
@@ -0,0 +1,61 @@
+import './style.css'
+import * as THREE from 'three'
+
+
+console.log('Hello World!')
+
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+
+// Scene
+const scene = new THREE.Scene()
+
+/**
+ * Objects
+ */
+const group = new THREE.Group()
+scene.add(group)
+
+
+const cube1 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+)
+
+group.add(cube1)
+
+
+
+//Axes Helper
+const axesHelper = new THREE.AxesHelper()
+scene.add(axesHelper)
+
+
+/**
+ * Sizes
+ */
+const sizes = {
+    width: 800,
+    height: 600
+}
+
+/**
+ * Camera
+ */
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+camera.position.z = 3
+scene.add(camera)
+
+
+
+//camera.lookAt(mesh.position)
+
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.render(scene, camera)
